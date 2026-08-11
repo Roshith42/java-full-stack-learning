7596d855c3da3d87e6200a33369bf04284304b3d
Sunkari Sai Roshith <sunkarisairoshith02@gmail.com>
2026-08-11 14:52:36 +0530

    Add Variables class with basic data types

---
+++ 01-Java-Basics/variales.java
@@ -0,0 +1,26 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // Integer
+        int age = 22;
+
+        // Decimal number
+        double salary = 35000.50;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean
+        boolean isStudent = true;
+
+        // String
+        String name = "Sai Roshith";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
